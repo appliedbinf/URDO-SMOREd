459b9172fa48d6c0278e86d813303bdac5ab3fca
Aroon Chande <git@atc.io>
2019-02-26 08:29:22 -0500

    No longer require the config flag when in predict mode closes #5

--- tests/test_load_config.py
+++ tests/test_load_config.py
@@ -8,3 +8,11 @@ def test_good_config():
 	abil_urdocaller.load_config('tests/config_good.txt')
 	assert True != False
 	assert abil_urdocaller.__config_dict__ == {'loci': {'alleles': 'tests/repDB-rename.fasta'}, 'profile': {'profile': 'tests/profile.txt'}}
+
+# def test_bad_config():
+# 	with pytest.raises(SystemExit) as pytest_wrapped_e:
+# 		abil_urdocaller.load_config('tests/config_bad.txt')
+# 	print(pytest_wrapped_e)
+# 	assert pytest_wrapped_e.type == SystemExit
+# 	assert pytest_wrapped_e.value.code == 1
+
